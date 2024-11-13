2b3eacbd6fb4e8a40dd009faf81e726d02ceaf7e
Deepender Singh <deepenrathore28@gmail.com>
2024-11-14 00:04:32 +0100

    airdrop

---
+++ airdrop.js
@@ -0,0 +1,18 @@
+import solanaWeb3, { clusterApiUrl, PublicKey } from '@solana/web3.js';
+import { Connection, LAMPORTS_PER_SOL } from '@solana/web3.js';
+
+const connection = new Connection(clusterApiUrl('devnet'));
+
+async function airdrop(publicKey, amount) {
+  const airdropSignature = await connection.requestAirdrop(
+    new PublicKey(publicKey),
+    amount
+  );
+  await connection.confirmTransaction({ signature: airdropSignature });
+}
+
+airdrop('6AXcCp7qziUoFamcbfZjvEfujyzxBUxx3jKTraCWVFkc', LAMPORTS_PER_SOL).then(
+  (signature) => {
+    console.log('Airdrop signature:', signature);
+  }
+);
